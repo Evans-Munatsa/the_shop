4d7e46c1ff93412dbb340d91ea884d6a5c8999cf
Evans-Munatsa <gagamel.em@gmail.com>
2016-10-31 09:22:49 +0200

    added using bycrypt

--- routes/users.js
+++ routes/users.js
@@ -5,30 +5,28 @@ exports.userSignup = function(req, res) {
     res.render('signup');
 };
 
-
 exports.register = function(req, res, next) {
     req.getConnection(function(err, connection) {
-
-
-        // console.log(req.body.email)
         if (req.body.name && req.body.email && req.body.password) {
-
             // if user puts correct info the get save to database
             var newUser = {
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password,
+                confirm_password: req.body.confirm_password
             };
 
             // using bycrypt to store hashed passwords
             var passwordStore = bcrypt.hash(newUser.password, saltRounds, function(err, hash) {
                 newUser.password = hash
-
-                connection.query('insert into Users set ?', newUser, function(err, results) {
-                    if (err) return next(err);
-                    req.flash("success", 'Welcome', newUser.name);
-                    req.session.user = newUser;
-                    res.redirect('/categories');
+                bcrypt.compare(newUser.confirm_password, hash, function(err, res) {
+                    newUser.confirm_password = hash
+                    connection.query('insert into Users set ?', newUser, function(err, results) {
+                        if (err) return next(err);
+                        req.flash("success", 'Welcome', newUser.name);
+                        req.session.user = newUser;
+                        res.redirect('/categories');
+                    });
                 });
             });
 
